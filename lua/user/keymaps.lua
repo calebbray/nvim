8e35f1d559c73033026e30f9302c7f97ff0eb8ae
Caleb Bray <caleb.bray@emsibg.com>
2023-01-20 11:36:47 -0800

    moved lua files into lua directory

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -15,6 +15,8 @@ vim.g.mapleader = " "
 --   term   = "t"
 --   command = "c"
 
+-- Redo better keymap
+keymap("n", "<S-u>", "<C-r>", opts)
 -- Better Window Navigation
 keymap("n", "<C-h>", "<C+w>h", opts)
 keymap("n", "<C-j>", "<C+w>j", opts)
@@ -24,10 +26,10 @@ keymap("n", "<C-l>", "<C+w>l", opts)
 keymap("n", "<leader>e", ":Lex 30<CR>", opts)
 
 -- Resize With Arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<S-Up>", ":resize -2<CR>", opts)
+keymap("n", "<S-Down>", ":resize +2<CR>", opts)
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate Buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
@@ -43,8 +45,8 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down -- NOT WORKING RIGHT NOW
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Better paste
 keymap("v", "p", '"_dP', opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,19 @@ return packer.startup(function(use)
   
   -- Colorschemes 
   use "lunarvim/darkplus.nvim"
+
+  -- cmp plugins
+  use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
+  use { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
+  use { "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" } -- path completions
+  use { "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
+  use { "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" }
+  use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
+
+  -- snippets
+  use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
+  use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+  
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
