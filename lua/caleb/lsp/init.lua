c8944b77e6d300cd6e76f74f8815afa20a11955a
Caleb Bray <caleb.bray@emsibg.com>
2023-01-23 10:46:48 -0800

    fix breaks from renaming user dir

--- lua/caleb/lsp/init.lua
+++ lua/caleb/lsp/init.lua
@@ -3,6 +3,6 @@ if not status_ok then
   return
 end
 
-require "user.lsp.mason"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require "caleb.lsp.mason"
+require("caleb.lsp.handlers").setup()
+require "caleb.lsp.null-ls"
