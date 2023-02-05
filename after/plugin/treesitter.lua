e1551866181557705493032be1ceecc3e97330bf
Caleb Bray <caleb.bray@emsibg.com>
2023-02-05 08:20:34 -0800

    commit plugins

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,31 @@
+local status_ok = pcall(require, "nvim-treesitter")
+if not status_ok then
+  print "Treesitter not available..."
+  return
+end
+
+local configs_status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not configs_status_ok then
+  print "Treesitter configs not available..."
+end
+
+configs.setup {
+  ensure_installed = { "lua", "markdown", "bash", "typescript", "javascript", "tsx", "rust" }, -- put the language you wnat in this array
+  ignore_install = { "" },
+  sync_install = false,
+  highlight = {
+    enable = true,
+    disable = { "css" },
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+    disable = { "css" },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+  },
+}
--- lua/caleb/lsp/mason.lua
+++ lua/caleb/lsp/mason.lua
@@ -7,6 +7,7 @@ local servers = {
   "jsonls",
   "yamlls",
   "gopls",
+  "rust_analyzer",
 }
 
 local settings = {
